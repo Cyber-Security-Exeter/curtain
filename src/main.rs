eae6feb57a4b4055acdeda5dfe04a42b32f65011
sopho-s <woodsnicholas01@gmail.com>
2026-07-26 18:54:48 +0100

    idk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
-fn main() {
-    println!("welcome to curtain");
+use tokio::{self, fs::read};
+use axum::{
+    routing::get,
+    Router,
+    Json
+};
+use axum::http::StatusCode;
+use axum::response::Html;
+use axum_extra::response::Css;
+pub mod filereader;
+use filereader::read_file;
+use serde::Deserialize;
+use rusqlite::{params, Connection, Result};
+
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+    email: String,
+    password: String,
+}
+
+async fn create_user(Json(user): Json<CreateUser>) {
+    let conn = Connection::open("userdata.db")?;
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL
+        )",
+        [],
+    );
+    conn.execute(
+        "INSERT INTO users (username, email, password) VALUES (?1, ?2, ?3)",
+        params![(&user).username, (&user).email, (&user).password],
+    );
+}
+
+
+async fn root() -> Html<String> {
+    let file = read_file("./pages/welcome.html");
+    Html(file)
+}
+
+async fn register() -> Html<String> {
+    let file = read_file("./pages/register.html");
+    Html(file)
+}
+
+async fn general() -> Css<String> {
+    let file = read_file("./pages/static/general.css");
+    Css(file)
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/static/general.css", get(general))
+        .route("/register", get(register));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
