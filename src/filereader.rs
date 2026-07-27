a3dd0407ad9d9a3f6717530cf6a8491a89f06b6a
sopho-s <woodsnicholas01@gmail.com>
2026-07-27 01:22:25 +0100

    new stuff

--- src/filereader.rs
+++ src/filereader.rs
@@ -1,6 +1,5 @@
 use std::fs;
 
 pub fn read_file(filename: &str) -> String {
-    println!("{}", filename);
     return fs::read_to_string(filename).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -11,30 +11,43 @@ pub mod filereader;
 use filereader::read_file;
 use serde::Deserialize;
 use rusqlite::{params, Connection, Result};
+use rusqlite::fallible_streaming_iterator::FallibleStreamingIterator;
+use uuid::Uuid;
 
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct CreateUser {
     username: String,
     email: String,
     password: String,
 }
 
-async fn create_user(Json(user): Json<CreateUser>) {
-    let conn = Connection::open("userdata.db").unwrap();
+async fn create_user(Json(user): Json<CreateUser>) -> Json<String> {
+    let conn: Connection = Connection::open("userdata.db").unwrap();
     conn.execute(
         "CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT NOT NULL,
+            id TEXT PRIMARY KEY,
+            username TEXT NOT NULL UNIQUE,
             email TEXT NOT NULL UNIQUE,
             password TEXT NOT NULL
         )",
         [],
+    ).unwrap();
+    let id = Uuid::new_v4();
+    let creation_result: Result<usize> = conn.execute(
+        "INSERT INTO users (id, username, email, password) VALUES (?1, ?2, ?3, ?4)",
+        (format!("{}", id.as_u128()), &user.username, &user.email, &user.password),
     );
-    conn.execute(
-        "INSERT INTO users (username, email, password) VALUES (?1, ?2, ?3)",
-        (&user.username, &user.email, &user.password),
-    );
+    if creation_result.is_err() {
+        let mut stmt = conn.prepare("SELECT id FROM users WHERE username=?1 OR email=?2").unwrap();
+        let rows = stmt.query([user.username, user.email]).unwrap();
+        let (size, _) = rows.size_hint();
+        if size > 0 as usize {
+            return Json("{\"status\": \"email or username is already registered\"}".to_owned());
+        }
+        return Json("{\"status\": \"email or username is already registered\"}".to_owned());
+    }
+    Json(format!("{{\"status\": \"ok\", \"jwt\":\"{}\"}}").to_owned())
 }
 
 
